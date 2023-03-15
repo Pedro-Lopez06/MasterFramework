eb31cd422b9a3db891aaf40381ffcc2b24d58b43
pedro-lopez06 <pedro.lopez3975@alumnos.udg.mx>
2023-03-14 20:19:03 -0600

    conexion bd

---
+++ Backend/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/api_rest_blog',{ useNewUrlParser: true })
+.then(()=>{
+    console.log('Conexión en la base de datos exitosa');
+});
